38f1023868460d87343b9c10cdfabacdcadf1b91
swilskey <swilskey41@gmail.com>
2015-09-19 09:13:51 -0700

    initial project file

---
+++ Instasite/Instasite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instasite
+//
+//  Created by Sam Wilskey on 9/19/15.
+//  Copyright © 2015 Instasite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Instasite/Instasite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instasite
+//
+//  Created by Sam Wilskey on 9/19/15.
+//  Copyright © 2015 Instasite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
