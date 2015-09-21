f0c2eb2fb867db75bd3ef6e9130c881b20c3dde9
Joao <joaopaulogalvao1@gmail.com>
2015-09-21 11:45:22 -0700

    Added keys.

---
+++ Instasite/Instasite/Keys.h
@@ -0,0 +1,13 @@
+//
+//  Keys.h
+//  Instasite
+//
+//  Created by Joao Paulo Galvao Alves on 9/21/15.
+//  Copyright © 2015 Instasite. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Keys : NSObject
+
+@end
---
+++ Instasite/Instasite/Keys.m
@@ -0,0 +1,15 @@
+//
+//  Keys.m
+//  Instasite
+//
+//  Created by Joao Paulo Galvao Alves on 9/21/15.
+//  Copyright © 2015 Instasite. All rights reserved.
+//
+
+#import "Keys.h"
+
+@implementation Keys
+
+
+
+@end
