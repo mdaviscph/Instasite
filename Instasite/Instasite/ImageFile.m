90bb0435998aca3ba92044fdfea3083308b6f443
swilskey <swilskey41@gmail.com>
2015-09-24 16:09:02 -0700

    Added classes for Images and Css

---
+++ Instasite/Instasite/ImageFile.m
@@ -0,0 +1,16 @@
+//
+//  ImageFile.m
+//  Instasite
+//
+//  Created by Sam Wilskey on 9/24/15.
+//  Copyright © 2015 Instasite. All rights reserved.
+//
+
+#import "ImageFile.h"
+
+@implementation ImageFile
+- (instancetype)init:(NSString *)filePath fileName:(NSString *)fileName {
+  self.fileName = fileName;
+  self.filePath = filePath
+}
+@end
